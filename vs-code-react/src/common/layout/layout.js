36e711245ff46e50dfbe1661306fe423e984cdfa
seoulit63 <xodbs753@gmail.com>
2020-08-25 16:33:07 +0900

    2020-08-25 layout.js 변경

--- vs-code-react/src/common/layout/layout.js
+++ vs-code-react/src/common/layout/layout.js
@@ -24,7 +24,7 @@ function Layout(props) {
   var classes = useStyles();
 
   // global
-  var layoutState = useLayoutState();  
+  var layoutState = useLayoutState();
 
   return (
     <div className={classes.root}>
@@ -42,6 +42,8 @@ function Layout(props) {
             {/* 메인 홈 : 여기도 이상없이 잘됨 */}
             <Route path="/app/dashboard" component={Dashboard} />
             {/* 각자 업무에 맞게 라우터를 정의 하시오 */}
+
+
             {/* 회 계 */}
             {/* 전표입력 */}
             <Route
@@ -57,6 +59,12 @@ function Layout(props) {
             />{" "}
             {/* 전표승인 */}
             {/* 계정과목 및 계정 */}
+            {/* 계정과목 */}
+            <Route
+              exact
+              path="/app/acc/base/accountForm"
+              component={account.AccountForm}
+            />{" "}
             {/* 거래처원장 */}
             <Route
               exact
@@ -64,13 +72,28 @@ function Layout(props) {
               component={account.FinancialStatements}
             />{" "}
             {/* 재무상태표 */}
+            {/* 합계잔액시산표 */}
             <Route
               exact
               path="/app/acc/statement/totalTrialBalance"
               component={account.TotalTrialBalance}
             />{" "}
-            {/* 합계잔액시산표 */}
-            {/* 전기분손익계산서 */}
+            {/*********************************** 2020-08-24 조편백 추가 ***********************************/}
+            {/* 손익계산서 */}
+            <Route
+              exact
+              path="/app/acc/statement/IncomeStatement"
+              component={account.IncomeStatement}
+            />{" "}
+            {/*********************************** 2020-08-24 조편백 끝 ***********************************/}
+            {/*********************************** 2020-08-24 정대현 추가 ***********************************/}
+            {/* 현금출납장 */}
+            <Route
+              exact
+              path="/app/acc/statement/cashJournal"
+              component={account.CashJournal}
+            />
+            {/*********************************** 2020-08-24 정대현 끝 ***********************************/}
             {/* 매입매출장 */}
             {/* 분개장 */}
             <Route
@@ -78,6 +101,18 @@ function Layout(props) {
               path="/app/acc/account/journalForm"
               component={account.JournalForm}
             />
+            {/* ///////////////////////// 2020-08-24 김진호 추가 /////////////////////////// */}
+            {/* 일(월)계표 */}
+            <Route
+              exact
+              path="/app/acc/statement/detailTrialBalance"
+              component={account.DetailTrialBalance}
+            />{" "}
+            {/* ///////////////////////// 2020-08-24 김진호 끝  /////////////////////////// */}
+
+
+
+
             {/* 인 사 */}
             {/* 인사관리 */}
             <Route
@@ -109,28 +144,50 @@ function Layout(props) {
             <Route
               exact
               path="/app/hr/attendance/dayAttendanceManage"
-              component={hr.DayAttdManage}
+              component={hr.DayAttdManageContainer}
             />{" "}
             {/* 일근태관리 */}
             <Route
               exact
               path="/app/hr/attendance/monthAttendanceManage"
-              component={hr.MonthAttendanceManage}
+              component={hr.MonthAttdManageContainer}
             />{" "}
             {/* 월근태관리 */}
             <Route
               exact
               path="/app/hr/attendance/monthAttendanceManage"
-              component={hr.MonthAttendanceManage}
+              component={hr.MonthAttdManageContainer}
             />{" "}
+           
             {/* 급여조회 */}
+            {/* 월급여 조회 */}
+            {/*******************2020-08-20 손유찬*************************/}
+            <Route
+              exact
+              path="/app/hr/Salary/monthSalary"
+              component={hr.MonthSalaryManage}
+            />{" "}
+            {/*******************2020-08-20 손유찬*************************/}
             {/* 급여관리 */}
+            {/* 월급여마감 */}
             <Route
               exact
               path="/app/hr/salary/closeSalary"
               component={hr.CloseSalaryContainer}
             />{" "}
-            {/* 물 류 */}
+            {/* 초과수당관리 */}
+            {/*******************2020-08-22 손유찬*************************/}
+            <Route
+              exact
+              path="/app/hr/salary/baseExtSalManage"
+              component={hr.BaseExtSalManage}
+            />{" "}
+            {/*******************2020-08-22 손유찬*************************/}
+
+
+
+
+        {/* 물 류 */}
             {/* 기초정보관리 */}
             {/* 물류정보관리 */}
             {/* 영업관리 */}
@@ -180,6 +237,11 @@ function Layout(props) {
               component={logistic.WorkInstruction}
             />{" "}
             {/* MPS */}
+
+
+
+
+
             {/* 부가기능 Route 여긴 이상없이 잘됨 */}
             <Route path="/app/typography" component={additional.Typography} />
             <Route path="/app/tables" component={additional.Tables} />
